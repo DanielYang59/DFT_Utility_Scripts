b0f552663d1d8bc90b25187aabe3ba10d435e22b
Haoyu Yang <yanghaoyu97@outlook.com>
2023-09-05 13:07:16 +0800

    correct mesh name,improve mesh selection

--- scripts/kpoints/README.md
+++ scripts/kpoints/README.md
@@ -1,8 +1,9 @@
+
 # VASP KPOINTS File Generator
 
 ## Overview
 
-This Python script generates a regular KPOINTS file for VASP calculations. The user can specify the number of k-points along each of the three axes (`a`, `b`, `c`) and can also choose between a Gamma-centered or Monkhorst-Pack mesh type.
+This Python script generates a regular KPOINTS file for VASP calculations. The user specifies the number of k-points along each of the three axes (`a`, `b`, `c`). They can also choose between a Gamma-centered or Monkhorst-Pack mesh type.
 
 ## Requirements
 
@@ -13,5 +14,5 @@ This Python script generates a regular KPOINTS file for VASP calculations. The u
 ### Syntax
 
 ```bash
-python3 kpoints.py <a> <b> <c> [-m <mesh_type>]
+python3 kpoints.py <a> <b> <c> [-m <mesh_type_initial>]
 ```
--- scripts/kpoints/generate_kpoints.py
+++ scripts/kpoints/generate_kpoints.py
@@ -11,55 +11,45 @@ def main():
     Generate a VASP KPOINTS file based on the given command-line arguments.
 
     The user can specify the number of k-points along each of the three axes (a, b, c),
-    and can optionally choose between a Gamma-centered or Monkhorst-Pack mesh type.
-
-    Examples:
-    ---------
-    Generate a Gamma-centered mesh with 4 k-points along each axis:
-    python3 kpoints.py 4 4 4
-
-    Generate a Monkhorst-Pack mesh with 4 k-points along each axis:
-    python3 kpoints.py 4 4 4 -m Monkhorst-Pack
+    and can optionally choose between a Gamma-centered and Monkhorst-Pack mesh type.
     """
     # Initialize argparse
     parser = argparse.ArgumentParser(description="Generate a regular KPOINTS file for VASP calculations.")
 
 
     # Positional arguments for k-points along the three axes
-    parser.add_argument("a", type=int, help="Number of subdivisions along the first reciprocal lattice vector")
-    parser.add_argument("b", type=int, help="Number of subdivisions along the second reciprocal lattice vector")
-    parser.add_argument("c", type=int, help="Number of subdivisions along the third reciprocal lattice vector")
+    parser.add_argument("kpoints", type=int, nargs=3, help="Three integers specifying subdivisions along reciprocal lattice vectors (a b c)")
 
 
     # Optional argument for mesh type
-    parser.add_argument("-m", "--mesh", choices=["gamma", "Monkhorst-Pack"], default="gamma", help="Type of mesh. Default is Gamma-centered.")
+    parser.add_argument("-m", "--mesh", choices=["g", "m"], default="g", help="Type of mesh ('g' for Gamma-centered, 'm' for Monkhorst-Pack). Default is 'g'.")
 
 
     # Parse arguments
     args = parser.parse_args()
 
 
-    # Check if k-points along each axis are greater than zero
-    if args.a <= 0 or args.b <= 0 or args.c <= 0:
-        raise ValueError("The number of k-points along each axis should be greater than zero.")
+    # Check if k-points are greater than zero
+    if any(k <= 0 for k in args.kpoints):
+        raise ValueError("All k-point values must be integers greater than zero.")
 
 
     # Set mesh type
-    mesh_type = "Gamma" if args.mesh == "gamma" else "Monkhorst-Pack"
+    mesh_type = "Gamma-centered" if args.mesh == "g" else "Monkhorst-Pack"
 
 
     # Generate KPOINTS file
-    kpoints_list = [args.a, args.b, args.c]
     kpoints_file_path = Path("KPOINTS")
 
     with kpoints_file_path.open("w") as f:
-        f.write("Regular k-point mesh\n")
-        f.write("0\n")
-        f.write(f"{mesh_type}\n")
-        f.write(f"{kpoints_list[0]}  {kpoints_list[1]}  {kpoints_list[2]}\n")
-        f.write("0  0  0\n")
+        f.write("Regular k-point mesh          ! Type of k-point grid\n")
+        f.write("0                              ! 0 -> determine number of k points automatically\n")
+        f.write(f"{mesh_type}                     ! generate a {mesh_type} mesh\n")
+        f.write(f"{args.kpoints[0]}  {args.kpoints[1]}  {args.kpoints[2]}         ! subdivisions N_1, N_2 and N_3 along the reciprocal lattice vectors\n")
+        f.write("0  0  0                         ! optional shift of the mesh (s_1, s_2, s_3)\n")
 
-    print(f"KPOINTS file generated with {mesh_type}-centered mesh and k-points: {kpoints_list}")
+
+    print(f"KPOINTS file generated with {mesh_type} mesh and k-points: {args.kpoints}")
 
 
 if __name__ == "__main__":
