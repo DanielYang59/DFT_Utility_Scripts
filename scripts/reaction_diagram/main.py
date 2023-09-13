e61d347ea9b2ef8a8637ca7640dd9718e3482457
Haoyu Yang <yanghaoyu97@outlook.com>
2023-09-13 18:35:45 +0800

    add docstring

--- scripts/reaction_diagram/main.py
+++ scripts/reaction_diagram/main.py
@@ -6,6 +6,14 @@ from src.reactionCalculator import reactionCalculator
 from src.generate_diagram import generate_reaction_diagram
 
 def main():
+    """
+    Main function to orchestrate the calculation and plotting of a reaction pathway diagram.
+
+    1. Reads molecular, intermediate, and reaction pathway data.
+    2. Initializes a reactionCalculator object with the read data.
+    3. Calculates the free energy change for each step in the reaction pathway.
+    4. Generates a diagram based on the calculated free energy changes.
+    """
     # Initialize reaction calculator
     calculator = reactionCalculator(read_molecular_species_csv("data/molecular_species_energy.csv"),
                                     read_intermediate_csv("data/intermediate_energy.csv"),
