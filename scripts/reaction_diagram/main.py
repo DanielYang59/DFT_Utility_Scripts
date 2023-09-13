fa065991e7616f1c4574d95cbe873f6a3e8986a0
Haoyu Yang <yanghaoyu97@outlook.com>
2023-09-13 19:51:53 +0800

    save diagram generator

--- scripts/reaction_diagram/main.py
+++ scripts/reaction_diagram/main.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from pathlib import Path
+
 from src.read_input import read_molecular_species_csv, read_intermediate_csv, read_reaction_data
 from src.reactionCalculator import reactionCalculator
-from src.generate_diagram import generate_reaction_diagram
+from src.diagramGenerator import ReactionDiagramGenerator
 
 def main():
+    """
+    Main function to orchestrate the calculation and plotting of a reaction pathway diagram.
+
+    1. Reads molecular, intermediate, and reaction pathway data.
+    2. Initializes a reactionCalculator object with the read data.
+    3. Calculates the free energy change for each step in the reaction pathway.
+    4. Generates a diagram based on the calculated free energy changes.
+    """
     # Initialize reaction calculator
     calculator = reactionCalculator(read_molecular_species_csv("data/molecular_species_energy.csv"),
                                     read_intermediate_csv("data/intermediate_energy.csv"),
@@ -15,8 +25,12 @@ def main():
     # Perform calculations
     deltaG_dict = calculator.interpret_reaction_pathway()
 
-    # Generate diagram
-    generate_reaction_diagram(deltaG_dict)
+    # Generate diagram using the new class
+    diagram_generator = ReactionDiagramGenerator(
+        deltaG_dict,
+        store_path=Path("output")/"reaction_diagram.png",
+        )
+    diagram_generator.generate()
 
 if __name__ == "__main__":
     main()
