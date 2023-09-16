3ea9a2b6af70c5c18bba77c84bbd01b793e7d4c2
Haoyu Yang <yanghaoyu97@outlook.com>
2023-09-16 19:12:39 +0800

    make script directly callable

--- scripts/poscar/coordinate_system_transfer.py
+++ scripts/poscar/coordinate_system_transfer.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from ase import Atoms
+from lib.utilities import read_poscar, write_poscar, find_or_request_poscar
 
 def cartesian_to_direct(atoms):
     """
@@ -44,3 +45,33 @@ def direct_to_cartesian(atoms):
                       pbc=atoms.get_pbc())
 
     return new_atoms
+
+def main():
+    """
+    Main function to prompt user for choice of coordinate conversion,
+    perform the conversion, and save the result to a new POSCAR file.
+    """
+    poscar_path = find_or_request_poscar()
+    atoms = read_poscar(poscar_path)
+
+    print("Select an operation:")
+    print("1: Cartesian to Direct")
+    print("2: Direct to Cartesian")
+    choice = input("Enter the number corresponding to your choice: ")
+
+    if choice == '1':
+        new_atoms = cartesian_to_direct(atoms)
+        output_filename = "POSCAR_direct"
+    elif choice == '2':
+        new_atoms = direct_to_cartesian(atoms)
+        output_filename = "POSCAR_cartesian"
+    else:
+        print("Invalid choice. Exiting.")
+        return
+
+    output_path = poscar_path.parent / output_filename
+    write_poscar(new_atoms, output_path)
+    print(f"Operation completed. New POSCAR file saved as {output_path}")
+
+if __name__ == "__main__":
+    main()
