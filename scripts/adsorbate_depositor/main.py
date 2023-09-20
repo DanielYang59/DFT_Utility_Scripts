bb68ecbdaeecb57cb716a989560bbff614f24901
Haoyu Yang <yanghaoyu97@outlook.com>
2023-09-20 20:21:16 +0800

    clean up main

--- scripts/adsorbate_depositor/main.py
+++ scripts/adsorbate_depositor/main.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from src.configHandler import ConfigHandler
 from src.adsorbateGenerator import AdsorbateGenerator
 from src.siteGenerator import SiteGenerator
-from src.structureGenerator import StructureGenerator
+from src.adsorbateDepositor import AdsorbateDepositor
 
 def main():
     # Read config.yaml or copy the template
@@ -15,24 +15,36 @@ def main():
     if cfg_handler.check_config_exists():
         config = cfg_handler.load_config()
     else:
-        cfg_handler.copy_config(template_path=Path("database") / "config_template.yaml")
+        cfg_handler.copy_config_template(template_path=Path("database") / "config_template.yaml")
 
     # Generate sites
-    site_generator = SiteGenerator()
-    sites = site_generator.generate_sites()
+    site_generator = SiteGenerator(
+        POSCAR_substrate=config["substrate"]["path"],
+        distance=config["deposit"]["distance"],
+        sites=config["substrate"]["sites"]
+        )
+    sites = site_generator.generate()
 
     # Generate adsorbates
-    adsorbate_generator = AdsorbateGenerator()
-    adsorbates = adsorbate_generator.generate_adsorbates()
+    adsorbate_generator = AdsorbateGenerator(generate_rotations=config["adsorbate"]["rotation"])
+    adsorbates = adsorbate_generator.generate(
+        work_mode=config["adsorbate"]["source"],
+        path=config["adsorbate"]["path"],
+        atom_indexes=config["adsorbate"]["atom_indexes"],
+        pathway_name=config["adsorbate"]["pathway_name"]
+        )
 
     # Generate adsorbate-on-site structure files
-    structure_generator = StructureGenerator(
-        distance=config["distance"],
-        auto_reposition=config["auto_reposition"]
+    structure_generator = AdsorbateDepositor(
+        POSCAR_substrate=config["substrate"]["path"],
+        sites=sites,
+        adsorbates=adsorbates,
+        auto_reposition_along_z=config["deposit"]["auto_reposition_along_z"],
+        distance=config["deposit"]["distance"],
+        output_dir=config["deposit"]["output_dir"]
         )
-    for site in sites:
-        for ads in adsorbates:
-            structure_generator.deposit(substrate_POSCAR, site, ads)
+
+    structure_generator.deposit()
 
 if __name__ == "__main__":
-    pass
+    main()
