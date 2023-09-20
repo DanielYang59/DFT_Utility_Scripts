1c16a9f181e680df4d7f5a80fcf939b6e88f9ecc
Haoyu Yang <yanghaoyu97@outlook.com>
2023-09-20 17:32:04 +0800

    move adsorbate POSCAR and indexes arg location

--- scripts/adsorbate_depositor/src/adsorbateGenerator.py
+++ scripts/adsorbate_depositor/src/adsorbateGenerator.py
@@ -13,68 +13,56 @@ class AdsorbateGenerator:
     Class for generating various configurations of adsorbates on a substrate.
     """
 
-    def __init__(self, POSCAR_adsorbate: Path, atom_indexes: List[int], generate_rotations: bool = False) -> None:
+    def __init__(self, generate_rotations: bool = False) -> None:
         """
         Initialize the AdsorbateGenerator object.
 
         Args:
-            POSCAR_adsorbate (Path): The path to the adsorbate POSCAR file.
-            atom_indexes (List[int]): List of atom indexes to extract (1-based indexing).
             generate_rotations (bool): Flag to generate rotated versions of adsorbate. Default is False.
 
         Raises:
-            FileNotFoundError: If the POSCAR file is not found.
             TypeError: If generate_rotations is not a boolean.
-            ValueError: If atom_indexes contains duplicate or illegal indexes.
         """
-        # Check args
-        if not POSCAR_adsorbate.is_file():
-            raise FileNotFoundError(f"Adsorbate POSCAR file {POSCAR_adsorbate} not found.")
-        if not isinstance(generate_rotations, bool):
-            raise TypeError("Illegal datatype for \"generate_rotations\"")
-
-        if len(atom_indexes) != len(set(atom_indexes)):
-            raise ValueError("Duplicate found in adsorbate atom selection.")
-        if not atom_indexes:
-            raise ValueError("Adsorbate atom selection is empty.")
-        for index in atom_indexes:
-            if index not in range(1, len(read(POSCAR_adsorbate, format="vasp")) + 1):
-                raise ValueError(f"Illegal adsorbate index \"{index}\" (indexing starts from 1).")
-
-        if not POSCAR_adsorbate.is_file():
-            raise FileNotFoundError(f"Adsorbate POSCAR file {POSCAR_adsorbate} not found.")
-
         if not isinstance(generate_rotations, bool):
             raise TypeError("Illegal datatype for \"generate_rotations\".")
 
-        if len(atom_indexes) != len(set(atom_indexes)):
-            raise ValueError("Duplicate found in adsorbate atom selection.")
-
-        if len(atom_indexes) == 0:
-            raise ValueError("Adsorbate atom selection is empty.")
-
-        for index in atom_indexes:
-            if index not in range(1, len(read(POSCAR_adsorbate, format="vasp")) + 1):
-                raise ValueError(f"Illegal adsorbate index \"{index}\" (indexing starts from 1).")
-
-        self.poscar = read(POSCAR_adsorbate, format="vasp")
-        self.atom_indexes = atom_indexes
         self.generate_rotations = generate_rotations
 
-    def _extract_atoms(self) -> Atoms:
+    def _extract_atoms(self, POSCAR_adsorbate: Path, atom_indexes: List[int]) -> Atoms:
         """
-        Extracts specified atoms from a POSCAR file.
+        Extracts specified atoms from the loaded POSCAR file.
+
+        Args:
+            POSCAR_adsorbate (Path): The path to the adsorbate POSCAR file.
+            atom_indexes (List[int]): List of atom indexes to extract (1-based indexing).
+
+        Raises:
+            FileNotFoundError: If the POSCAR file is not found.
+            ValueError: If atom_indexes contains duplicate or illegal indexes.
 
         Returns:
             Atoms: ASE Atoms object containing the extracted adsorbate atoms.
         """
-        # Correct index list (ASE uses 0-based indexing, while user provided 1-based indexing)
-        corrected_index_list = [index - 1 for index in self.poscar]
+        if not POSCAR_adsorbate.is_file():
+            raise FileNotFoundError(f"Adsorbate POSCAR file {POSCAR_adsorbate} not found.")
 
-        # Extract specified atoms
-        return self.poscar[corrected_index_list]
+        if len(atom_indexes) != len(set(atom_indexes)):
+            raise ValueError("Duplicate found in adsorbate atom selection.")
 
-    def _generate_rotations(self, adsorbate_atoms):
+        if not atom_indexes:
+            raise ValueError("Adsorbate atom selection is empty.")
+
+        # Import POSCAR
+        poscar = read(POSCAR_adsorbate, format="vasp")
+        for index in atom_indexes:
+            if index not in range(1, len(poscar) + 1):
+                raise ValueError(f"Illegal adsorbate index \"{index}\" (indexing starts from 1).")
+
+        # Extract adsorbate atoms
+        corrected_index_list = [index - 1 for index in atom_indexes]
+        return poscar[corrected_index_list]
+
+    def _generate_rotations(self, adsorbate_atoms: Atoms) -> List[Atoms]:
         """
         Generate rotated versions of the given adsorbate.
 
@@ -90,17 +78,16 @@ class AdsorbateGenerator:
             raise ValueError("Empty adsorbate fed into adsorbate rotation generator.")
 
         elif len(adsorbate_atoms) == 1:
-            warnings.warn("Adsorbate containing only one atom, rotation skipped.")
+            warnings.warn("Adsorbate containing only one atom; rotation skipped.")
             return [adsorbate_atoms, ]
 
         else:
-            # Define rotations: (axis, angle)
             rotations = [
                 ("x", 90),  # Rotate 90 degrees about x
                 ("x", -90), # Rotate -90 degrees about x
                 ("y", 90),  # Rotate 90 degrees about y
                 ("y", -90), # Rotate -90 degrees about y
-                ("z", 180)  # Rotate 180 degrees about z (opposite face)
+                ("z", 180), # Rotate 180 degrees about z
             ]
 
             rotated_adsorbates = []
@@ -111,14 +98,28 @@ class AdsorbateGenerator:
 
             return rotated_adsorbates
 
-    def generate(self, work_mode="POSCAR"):
-        assert work_mode in {"POSCAR", "DATABASE"}
+    def generate(self, work_mode: str = "POSCAR"):
+        """
+        Generate adsorbates based on the working mode.
 
+        Args:
+            work_mode (str): The working mode ("POSCAR" or "DATABASE").
+
+        Raises:
+            RuntimeError: If an illegal working mode is passed.
+        """
+        if work_mode == "POSCAR":
+            pass  # TODO
+
+        elif work_mode == "DATABASE":
+            pass  # TODO
+
+        else:
+            raise RuntimeError(f"Illegal working mode {work_mode} for adsorbate generator.")
 
 # Test area
 if __name__ == "__main__":
     adsorbate_generator = AdsorbateGenerator(
-        POSCAR_adsorbate="",
+        POSCAR_adsorbate=Path("path/to/your/POSCAR/file"),
+        atom_indexes=[1, 2, 3],
         generate_rotations=True)
-
-    adsorbates = adsorbate_generator.generate()
