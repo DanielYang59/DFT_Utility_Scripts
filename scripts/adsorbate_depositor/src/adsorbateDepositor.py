d2ed3f2de95ff297a90c50848acde4104cb846d9
Haoyu Yang <yanghaoyu97@outlook.com>
2023-09-21 18:54:41 +0800

    finish adsorbateDepositor

--- scripts/adsorbate_depositor/src/adsorbateDepositor.py
+++ scripts/adsorbate_depositor/src/adsorbateDepositor.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from typing import Union, List
+from typing import List
 from pathlib import Path
 import warnings
 from tqdm import tqdm
 from ase import Atoms
-from ase.io import read
+from ase.io import read, write
+from ase.constraints import FixAtoms
+
+TAG_DESCRIPTIONS = {
+    0: "substrate",
+    1: "adsorbate"
+}
 
 class AdsorbateDepositor:
     """
@@ -55,10 +61,20 @@ class AdsorbateDepositor:
         self.adsorbates = adsorbates
         self.adsorbate_refs = adsorbate_refs
 
-    def _fix_substrate(self, poscar: Atoms, substrate_indexes: List[int]):
+    def _fix_substrate(self, poscar: Atoms, substrate_indexes: List[int]) -> Atoms:
         """
-        Fix selected atoms for selective dynamics.
+        Fix atoms tagged as substrate (tag=0) for selective dynamics.
+
+        Args:
+            poscar (Atoms): The Atoms object representing the structure.
+
+        Returns:
+            Atoms: The Atoms object with fixed substrate atoms.
+
+        Notes:
+            - The function uses integer tags to identify atoms. Specifically, atoms with tag 0 are considered 'substrate' and atoms with tag 1 are considered 'adsorbate'.
         """
+
         # Type checks
         if not isinstance(poscar, Atoms):
             raise TypeError(f"Expected 'poscar' to be of type Atoms, but got {type(poscar)}.")
@@ -70,35 +86,34 @@ class AdsorbateDepositor:
         if any(i < 1 for i in substrate_indexes):
             raise ValueError("All substrate indexes must be >= 1 (should be 1-indexed).")
 
-    def _set_vacuum_level(self, new_vacuum_level: Union[float, int]):
-        """
-        Set vacuum level thickness.
-        """
-        # TODO: warning for 1st incomplete version
-        warnings.warn("Current 1st version of set vacuum level is only able to handle cases where atom cluster is continuous along z-axis (vacuum level not reside on the middle of the model).")
+        # Define tag description
+        tag_descriptions = {
+            0: "substrate",
+            1: "adsorbate"
+        }
 
-        # Check the type of new_vacuum_level
-        if not isinstance(new_vacuum_level, (float, int)):
-            raise TypeError(f"Expected 'new_vacuum_level' to be of type float or int, but got {type(new_vacuum_level)}.")
+        # Get indices of substrate atoms
+        substrate_indices = [atom.index for atom in poscar if atom.tag == 0]  # 0 is the tag for "substrate"
 
-        # Check the value of new_vacuum_level
-        if new_vacuum_level <= 0:
-            raise ValueError("Vacuum level thickness must be greater than zero.")
+        # Set constraints to fix substrate atoms
+        constraint = FixAtoms(indices=substrate_indices)
+        poscar.set_constraint(constraint)
 
-        # Issue a warning for small vacuum levels
-        elif new_vacuum_level <= 5:
-            warnings.warn(f"Small vacuum level of {new_vacuum_level} Å set.")
+        return poscar
 
-    def _reposition_along_z(self):
-        """
-        Reposition atom cluster along z-axis.
-        """
-        # TODO: warning for 1st incomplete version
-        warnings.warn("Current 1st version of atom reposition is only able to handle cases where atom cluster is continuous along z-axis (vacuum level not reside on the middle of the model).")
-
-    def _offset_adsorbate_along_z(self, minimal_distance: Union[float, int], direction: str = "top"):
+    def _offset_adsorbate_along_z(self, poscar: Atoms, minimal_distance: float = 1.5) -> Atoms:
         """
         Offset adsorbate along z-axis to maintain minimal distance.
+
+        Args:
+            poscar (Atoms): The Atoms object.
+            minimal_distance (float): The minimal distance to maintain along the Z-axis.
+
+        Returns:
+            Atoms: The Atoms object with offset adsorbate.
+
+        Notes:
+            - This function only offsets atoms with tag 1, which are considered 'adsorbate'.
         """
         # Check the type of minimal_distance
         if not isinstance(minimal_distance, (float, int)):
@@ -112,33 +127,67 @@ class AdsorbateDepositor:
         elif minimal_distance <= 1:
             warnings.warn(f"Small adsorbate-substrate distance of {minimal_distance} Å set.")
 
-        # Check the value of direction
-        if direction not in {"top", "bottom"}:
-            raise ValueError(f"Adsorbate offset direction should be either 'top' or 'bottom', but got {direction}.")
+        # Filter only adsorbate atoms (tag=1)
+        adsorbate_atoms = [atom for atom in poscar if atom.tag == 1]
 
-    def _deposit_adsorbate_on_site(self, poscar_substrate: Atoms, site: List[float], adsorbate: Atoms, ads_reference: List[int], auto_offset_along_z: bool = True) -> Atoms:
+        if not adsorbate_atoms:
+            return poscar
+
+        # Determine the minimum z-coordinate among the adsorbate atoms
+        z_min = min(atom.position[2] for atom in adsorbate_atoms)
+
+        # Calculate the amount by which to offset the adsorbate atoms along z
+        offset = minimal_distance - z_min
+
+        # Apply the offset to adsorbate atoms
+        for atom in adsorbate_atoms:
+            atom.position[2] += offset
+
+        return poscar
+
+    def _deposit_adsorbate_on_site(self, poscar_substrate: Atoms, site: List[float], adsorbate: Atoms, ads_reference: List[int]) -> Atoms:
         """
-        The actual worker that deposit adsorbate onto substrate site.
-        NOTE: be sure NOT to sort the atom list (keep adsorbate to the end of POSCAR).
+        Deposits the adsorbate onto the substrate site.
+
+        Args:
+            poscar_substrate (Atoms): The Atoms object of the substrate.
+            site (List[float]): The coordinates of the adsorption site.
+            adsorbate (Atoms): The Atoms object of the adsorbate.
+            ads_reference (List[int]): List of atom indexes to use as the reference for positioning.
+
+        Returns:
+            Atoms: The resulting Atoms object after adsorbate deposition.
+
+        Notes:
+            - Atoms with tag 0 are considered 'substrate' and atoms with tag 1 are considered 'adsorbate'.
         """
-        # Check args:  # TODO: clean up
-        assert isinstance(poscar_substrate, Atoms)
-        assert isinstance(site, list) and len(site) == 3
-        assert isinstance(adsorbate, Atoms)
-        assert isinstance(ads_reference, list)
+        # Check args
+        if not isinstance(poscar_substrate, Atoms):
+            raise TypeError("Wrong datatype for substrate POSCAR.")
+        if not isinstance(adsorbate, Atoms):
+            raise TypeError("Wrong datatype for adsorbate POSCAR.")
+        if not isinstance(site, list) or len(site) != 3:
+            raise RuntimeError("Wrong site datatype or length.")
+        if not isinstance(ads_reference, list):
+            raise TypeError("Wrong adsorbate reference point list datatype.")
 
-        # Combine substrate and adsorbate POSCARs
+        # Tag all substrate atoms with 0
+        for atom in poscar_substrate:
+            atom.tag = 0
 
-        # Calculate adsorbate moving vector
+        # Tag all adsorbate atoms with 1
+        for atom in adsorbate:
+            atom.tag = 1
 
-    def deposit(self, target_vacuum_level: Union[float, int], auto_offset_along_z: bool = True, center_along_z: bool = True, fix_substrate: bool = False) -> dict:
+        # Combine substrate and adsorbate
+        return poscar_substrate + adsorbate
+
+    def deposit(self, auto_offset_along_z: bool = True, fix_substrate: bool = False) -> dict:
         """
         Deposit adsorbates onto specified sites on the substrate.
 
         Args:
-            target_vacuum_level (Union[float, int]): The target vacuum level in angstroms.
             auto_offset_along_z (bool, optional): Whether to automatically offset the adsorbate along the Z-axis. Defaults to True.
-            center_along_z (bool, optional): Whether to center the structure along the Z-axis. Defaults to True.
             fix_substrate (bool, optional): Whether to fix the substrate atoms during deposition. Defaults to False.
 
         Returns:
@@ -147,19 +196,10 @@ class AdsorbateDepositor:
         Raises:
             TypeError: If the provided arguments are not of the expected types.
         """
-        # Check the type and value of target_vacuum_level
-        if not isinstance(target_vacuum_level, (float, int)):
-            raise TypeError(f"Expected 'target_vacuum_level' to be of type float or int, but got {type(target_vacuum_level)}.")
-
-        elif target_vacuum_level <= 5:
-            warnings.warn(f"Small vacuum level of {target_vacuum_level} Å set.")
 
         # Check the type of boolean flags
         if not isinstance(auto_offset_along_z, bool):
             raise TypeError(f"Expected 'auto_offset_along_z' to be of type bool, but got {type(auto_offset_along_z)}.")
-
-        if not isinstance(center_along_z, bool):
-            raise TypeError(f"Expected 'center_along_z' to be of type bool, but got {type(center_along_z)}.")
 
         if not isinstance(fix_substrate, bool):
             raise TypeError(f"Expected 'fix_substrate' to be of type bool, but got {type(fix_substrate)}.")
@@ -175,10 +215,12 @@ class AdsorbateDepositor:
                 result = self._deposit_adsorbate_on_site(self.poscar_substrate, site_info, ads_info, ads_reference, auto_offset_along_z)
 
                 # Optional post-processing steps
-                if center_along_z:  # Center the atom cluster along the Z-axis
-                    result = self._reposition_along_z(result, mode="z_center")
+                ##  Offset adsorbate along z-axis
+                if auto_offset_along_z:
+                    self._offset_adsorbate_along_z(result, minimal_distance=2.0)
 
-                if fix_substrate:  # Freeze substrate atoms (for selective dynamics)
+                ## Freeze substrate atoms (for selective dynamics)
+                if fix_substrate:
                     result = self._fix_substrate(result, self.poscar_substrate)
 
                 # Compile the sample name from site and adsorbate names
@@ -189,17 +231,25 @@ class AdsorbateDepositor:
 
         return results
 
-    def write(self, output_dir: Path):
-
-        """Write generated dict to file.
+    def write(self, atoms: Atoms, output_dir: Path, filename: str = "POSCAR_generated"):
+        """
+        Write generated Atoms object to file.
 
         Args:
-            output_dir (Path): Path to the directory where output will be saved.
-
+            atoms (Atoms): The Atoms object to write.
+            output_dir (Path): Directory where the Atoms object will be saved.
+            filename (str, optional): Filename for the output. Defaults to "POSCAR_generated".
         """
+        # Check structure file datatype
+        if not isinstance(atoms, Atoms):
+            raise TypeError("Wrong datatype to write.")
+
         # Check and create the output directory
         if not isinstance(output_dir, Path):
             raise TypeError(f"Expected 'output_dir' to be of type Path, but got {type(output_dir)}.")
 
         if not output_dir.is_dir():
             output_dir.mkdir(parents=True)
+
+        # Write ase.Atoms to file
+        write(output_dir / filename, atoms, format="vasp")
