3fab37cab43aa36ae6d972c2b78cdf3185544ed1
Haoyu Yang <yanghaoyu97@outlook.com>
2023-09-22 11:48:17 +0800

    add validation logics

--- scripts/adsorbate_depositor/database/check_database.py
+++ scripts/adsorbate_depositor/database/check_database.py
@@ -1,28 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import yaml
-from pathlib import Path
 import argparse
+from pathlib import Path
+import yaml
 
 def read_yaml_file(file_path):
-    """
-    Reads a YAML file and returns the parsed data.
-    """
+    """Reads a YAML file and returns the parsed data."""
     try:
         with open(file_path, "r", encoding="utf-8") as file:
             return yaml.safe_load(file)
     except yaml.YAMLError as exc:
         print(f"Failed to read {file_path}.")
-        if hasattr(exc, "problem_mark"):
+        if hasattr(exc, 'problem_mark'):
             mark = exc.problem_mark
             print(f"Error position: (Line:{mark.line+1}, Column:{mark.column+1})")
         return None
 
 def check_database(header_filename="pathway_database_header.yaml"):
-    """
-    Checks the integrity and structure of the adsorbate database.
-    """
+    """Checks the integrity and structure of the adsorbate database."""
     passed = []
     failed = []
     warnings = []
@@ -34,13 +30,19 @@ def check_database(header_filename="pathway_database_header.yaml"):
             if header_file.exists():
                 data = read_yaml_file(header_file)
                 if data:
-                    # Add your validation logic here.
-                    # If validation passes:
-                    passed.append(str(header_file))
-                    # If validation fails:
-                    # failed.append(str(header_file))
+                    # Validation logic starts here
+                    for pathway, pathway_data in data.get("pathways", {}).items():
+                        # Check if the indices are continuous integers starting from 1
+                        steps = list(pathway_data.keys())[2:]  # Skip 'reference_DOI' and 'comment'
+                        indices = [int(s.split('_')[-1]) for s in steps]
+                        if indices != list(range(1, len(steps) + 1)):
+                            failed.append(f"{pathway} in {header_file} has non-continuous step indexing.")
+                            continue
+                        # More validation logic here.
+                        passed.append(str(header_file))
+                    # Validation logic ends here
                 else:
-                    failed.append(str(header_file) + " could not be read.")
+                    failed.append(f"Failed to read {header_file}.")
             else:
                 failed.append(f"{folder} does not contain a {header_filename}.")
 
