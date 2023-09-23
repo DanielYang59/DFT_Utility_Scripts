0be681d2f9f4d7a9e49c23772c97669e4175b56e
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-09-23 23:07:44 +0800

    refactor into class and main

--- scripts/vasp/potcar/generate_potcar.py
+++ scripts/vasp/potcar/generate_potcar.py
@@ -3,115 +3,134 @@
 
 import os
 from pathlib import Path
+import warnings
+from typing import List
+from periodictable import elements
 
-def generate_potcar(potcar_lib: Path, elements: list, output_potcarfile: Path = Path("POTCAR")) -> None:
+class PotcarGenerator:
     """
-    Generate a POTCAR file based on a list of elements.
+    PotcarGenerator class responsible for generating POTCAR files based on a list of elements.
 
-    Parameters:
+    Attributes:
         potcar_lib (Path): A Path object representing the directory where the POTCAR library resides.
-        elements (list): A list containing the chemical symbols of the elements.
-        output_potcarfile (Path): A Path object for the output POTCAR file. Defaults to 'POTCAR' in the current directory.
 
-    Raises:
-        FileNotFoundError: If POTCAR for any of the specified elements is not found in the library.
+    Methods:
+        generate_potcar(elements: List[str], output_potcarfile: Path = Path("POTCAR")) -> None:
+            Generates a POTCAR file based on a list of elements.
+        get_elements_from_poscar(poscarfile: Path) -> List[str]:
+            Retrieves the list of elements from a given POSCAR file.
+        get_potcar_lib() -> Path:
+            Obtains the POTCAR library path from the environment variable.
     """
-    # Initialize an empty string to store POTCAR data
-    potcar_data = ""
 
-    for element in elements:
-        element_potcar_path = potcar_lib / element / "POTCAR"
+    def __init__(self):
+        """
+        Initialize the PotcarGenerator class.
+        """
+        self.potcar_lib = self.get_potcar_library_path()
 
-        # Check if POTCAR file for the element exists
-        if not element_potcar_path.is_file():
-            raise FileNotFoundError(f"POTCAR for {element} not found in {potcar_lib}.")
+    def get_elements_from_poscar(self, poscarfile: Path) -> list:
+        """
+        Get the list of elements from a POSCAR file.
 
-        # Write output POTCAR file
-        with element_potcar_path.open() as f:
-            potcar_data += f.read()
+        Parameters:
+            poscarfile (Path): Path to the POSCAR file.
 
+        Returns:
+            list: List of elements.
 
-    # Write the combined POTCAR data to the output path
-    with output_potcarfile.open("w") as f:
-        f.write(potcar_data)
+        Raises:
+            FileNotFoundError: If the specified POSCAR file does not exist.
+            RuntimeError: If the elements list in the POSCAR file is empty.
+        """
+        if not poscarfile.is_file():
+            raise FileNotFoundError(f"The POSCAR file {poscarfile} does not exist.")
 
-def get_elements_from_poscar(poscarfile: Path) -> list:
-    """
-    Get the list of elements from a POSCAR file.
+        with poscarfile.open() as f:
+            lines = f.readlines()
+        elements = lines[5].strip().split()
 
-    Parameters:
-        poscarfile (Path): Path to the POSCAR file.
+        if not elements:
+            raise RuntimeError("The elements list is empty. Check your POSCAR file.")
 
-    Returns:
-        list: List of elements.
+        return elements
 
-    Raises:
-        FileNotFoundError: If the specified POSCAR file does not exist.
-        RuntimeError: If the elements list in the POSCAR file is empty.
-    """
-    # Check if POSCAR file exists
-    if not Path(poscarfile).is_file():
-        raise FileNotFoundError(f"The POSCAR file {poscarfile} does not exist.")
+    def get_potcar_library_path(self) -> Path:
+        """
+        Get the path to the POTCAR library from the environment variable.
 
-    # Get the 6th line in POSCAR and split the line into element symbols
-    with poscarfile.open() as f:
-        lines = f.readlines()
-    elements = lines[5].strip().split()
+        Returns:
+            Path: Path to the POTCAR library.
 
-    if not elements:
-        raise RuntimeError("The elements list is empty. Check your POSCAR file.")
+        Raises:
+            EnvironmentError: If the POTCAR_LIBRARY_PATH environment variable is not set.
+            FileNotFoundError: If the path specified in the environment variable does not exist or is not a directory.
+        """
+        POTCAR_LIBRARY_PATH = os.environ.get("POTCAR_LIBRARY_PATH")
+        if POTCAR_LIBRARY_PATH is None:
+            raise EnvironmentError(
+                "The POTCAR_LIBRARY_PATH environment variable is not set. "
+                "Please set this variable to the path of your POTCAR library."
+            )
 
-    return elements
+        potcar_path = Path(POTCAR_LIBRARY_PATH)
+        if not potcar_path.is_dir():
+            raise FileNotFoundError(
+                f"The path specified in POTCAR_LIBRARY_PATH ({POTCAR_LIBRARY_PATH}) either does not exist "
+                "or is not a directory. Please check the path and try again."
+            )
 
-def get_potcar_library_path() -> Path:
-    """
-    Get the path to the POTCAR library from the environment variable.
+        return potcar_path
 
-    Returns:
-        Path: Path to the POTCAR library.
+    def generate_potcar(self, elements: List[str], output_potcarfile: Path = Path("POTCAR"), max_elements: int = 50) -> None:
+        """
+        Generate a POTCAR file based on a list of elements.
 
-    Raises:
-        EnvironmentError: If the POTCAR_LIBRARY_PATH environment variable is not set.
-        FileNotFoundError: If the path specified in the environment variable does not exist or is not a directory.
-    """
-    # Get environment variable for path to POTCAR library
-    POTCAR_LIBRARY_PATH = os.environ.get("POTCAR_LIBRARY_PATH")
+        Parameters:
+            elements (list): A list containing the chemical symbols of the elements.
+            output_potcarfile (Path): A Path object for the output POTCAR file. Defaults to 'POTCAR' in the current directory.
 
-    # Throw error if env var is not set
-    if POTCAR_LIBRARY_PATH is None:
-        raise EnvironmentError(
-            "The POTCAR_LIBRARY_PATH environment variable is not set. "
-            "Please set this variable to the path of your POTCAR library. "
-            "For example, you can add 'export POTCAR_LIBRARY_PATH=/path/to/your/potcar/library' "
-            "to your shell configuration file (.bashrc, .zshrc, etc.)."
-        )
+        Raises:
+            FileNotFoundError: If POTCAR for any of the specified elements is not found in the library.
+        """
+        # Check for warnings related to large number of elements
+        if len(elements) > max_elements:
+            warnings.warn(f"The elements list contains more than {max_elements} elements. Proceed with caution.")
 
-    # Check if the path exists and is a directory
-    potcar_path = Path(POTCAR_LIBRARY_PATH)
+        # Check for valid elements
+        valid_elements = {el.symbol for el in elements}
+        for element in elements:
+            if element not in valid_elements:
+                raise ValueError(f"{element} is not a valid chemical element.")
 
-    if not potcar_path.is_dir():
-        raise FileNotFoundError(
-            f"The path specified in POTCAR_LIBRARY_PATH ({POTCAR_LIBRARY_PATH}) either does not exist "
-            "or is not a directory. Please check the path and try again."
-        )
+        # Initialize an empty string to store POTCAR data
+        potcar_data = ""
 
-    return potcar_path
+        for element in elements:
+            element_potcar_path = self.potcar_lib / element / "POTCAR"
 
-def main(poscarfile="POSCAR") -> None:
+            # Check if POTCAR file for the element exists
+            if not element_potcar_path.is_file():
+                raise FileNotFoundError(f"POTCAR for {element} not found in {self.potcar_lib}.")
+
+            # Write output POTCAR file
+            with element_potcar_path.open() as f:
+                potcar_data += f.read()
+
+        # Write the combined POTCAR data to the output path
+        with output_potcarfile.open("w") as f:
+            f.write(potcar_data)
+
+def main(poscarfile: Path = Path("POSCAR")) -> None:
     """
     Main function to create a POTCAR file based on elements listed in a POSCAR file.
 
     Parameters:
         poscarfile (Path): Path to the POSCAR file. Defaults to 'POSCAR' in the current directory.
     """
-    # Read and check POTCAR library path from environment variable
-    potcar_path = get_potcar_library_path()
-
-    # Get list of elements from POSCAR
-    elements = get_elements_from_poscar(poscarfile)
-
-    # Generate new POTCAR
-    generate_potcar(potcar_path, elements)
+    potcar_gen = PotcarGenerator()
+    elements = potcar_gen.get_elements_from_poscar(poscarfile)
+    potcar_gen.generate_potcar(elements)
 
 if __name__ == "__main__":
     main()
