d3970b3215a0e2aaf10e2da8a7955b5d967aece9
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-09-23 17:36:18 +0800

    add move continuous atoms method

--- scripts/vasp/poscar/reposition_along_z.py
+++ scripts/vasp/poscar/reposition_along_z.py
@@ -6,7 +6,7 @@ import numpy as np
 import argparse
 from ase.io import write
 
-from .vacuumLayerManager import VacuumLayerManager
+from vacuumLayerManager import VacuumLayerManager
 from lib.utilities import find_or_request_poscar, read_poscar
 
 class StructureRepositioner:
@@ -26,83 +26,76 @@ class StructureRepositioner:
         """
         self.structure = structure
 
-    def move_atoms_to_top(self):
+    def move_continuous_atoms(self, mode: str):
         """
-        Move atoms to the top of the cell.
-        """
-        z_coords = self.structure.positions[:, 2]
-        min_z = min(z_coords)
-        shift_value = self.structure.get_cell()[2, 2] - min_z
-        self.structure.positions[:, 2] += shift_value
-
-    def move_atoms_to_bottom(self):
-        """
-        Move atoms to the bottom of the cell.
-        """
-        z_coords = self.structure.positions[:, 2]
-        max_z = max(z_coords)
-        shift_value = max_z
-        self.structure.positions[:, 2] -= shift_value
-
-    def move_atoms_to_center(self):
-        """
-        Move the centroid of atoms to the center of the cell.
-        """
-        z_coords = self.structure.positions[:, 2]
-        centroid = np.mean(z_coords)
-        shift_value = self.structure.get_cell()[2, 2] / 2 - centroid
-        self.structure.positions[:, 2] += shift_value
-
-    def reposition_along_z(self, mode: str, check_vacuum_layer_numbers: bool = True):
-        """
-        Reposition atoms in the structure along the Z-axis based on the given mode.
+        Move atoms that are continuous to the specified position ('top', 'bottom', 'center') in the cell.
 
         Parameters:
-            mode (str): Specifies how atoms should be moved ("top", "bottom", "center", "centre").
-            check_vacuum_layer_numbers (bool, optional): Whether to check for a single vacuum layer. Default is True.
+            mode (str): Specifies where atoms should be moved ("top", "bottom", "center").
+        """
+        z_coords = self.structure.positions[:, 2]
+        cell_height = self.structure.get_cell()[2, 2]
+        centroid = np.mean(z_coords)
 
-        Raises:
-            RuntimeError: If the mode is unsupported or if more than one vacuum layer is found when check_vacuum_layer_numbers is True.
+        if mode == "top":
+            shift_value = cell_height - max(z_coords)
+        elif mode == "bottom":
+            shift_value = -min(z_coords)
+        elif mode in {"center", "centre"}:
+            shift_value = cell_height / 2 - centroid
+        else:
+            raise ValueError(f"Unsupported mode: {mode}")
+
+        self.structure.positions[:, 2] += shift_value
+
+    def move_split_atoms(self, mode: str) -> None:
+        """
+        Move atoms that are split by the vacuum layer to a continuous configuration.
+        This assumes atoms on top should go below atoms at the bottom.
+        """
+        # TODO:
+        pass
+
+    def reposition_along_z(self, mode: str) -> None:
+        """
+        Reposition atoms based on the specified mode and the presence of a vacuum layer.
+
+        Parameters:
+            mode (str): Specifies where atoms should be moved ("top", "bottom", "center", "split").
         """
         # Check work mode
-        if mode not in {"top", "bottom", "center", "centre"}:
-            raise RuntimeError(f"Unsupported work mode {mode} for reposition module.")
+        if mode not in {"top", "bottom", "center", "centre", "split"}:
+            raise ValueError(f"Unsupported work mode {mode}.")
 
-        # Check vacuum layer count and position
+        # Check vacuum layer position and count
         vacuum_manager = VacuumLayerManager(self.structure)
-        vacuum_layer_count = vacuum_manager.count_vacuum_layer()
         vacuum_layer_position = vacuum_manager.locate_vacuum_layer()
-        if check_vacuum_layer_numbers and vacuum_layer_count != 1:
-            raise RuntimeError(f"Expect structure with exactly one vacuum layer, {vacuum_layer_count} found.")
+        vacuum_layer_count = vacuum_manager.count_vacuum_layers()
 
-        # Move atoms
-        if mode == "top":
-            self.move_atoms_to_top()
-        elif mode == "bottom":
-            self.move_atoms_to_bottom()
+        if vacuum_layer_count >= 2:
+            raise RuntimeError("Atom repositioner cannot handle cases where there are more than one vacuum layers.")
+
+        # Move atoms accordingly
+        if vacuum_layer_position == "middle":
+            self.move_split_atoms(mode)
         else:
-            self.move_atoms_to_center()
+            self.move_continuous_atoms(mode)
 
 def main():
     """
     Main function to handle user input and execute atom repositioning.
     """
     parser = argparse.ArgumentParser(description="Reposition atoms in an ASE structure along the Z-axis.")
-    parser.add_argument("mode", type=str, choices=["top", "bottom", "center", "centre"], help="How to reposition the atoms ('top', 'bottom', 'center', 'centre').")
-    parser.add_argument("--check_vacuum_layer_number", default=True, type=bool, help="Check if there is only one vacuum layer. Default is True.")
+    parser.add_argument("mode", type=str, choices=["top", "bottom", "center", "centre", "split"], help="How to reposition the atoms.")
 
     args = parser.parse_args()
 
-    # Use find_or_request_poscar() to get the path to the POSCAR file.
     structure_path = find_or_request_poscar()
-
-    # Read the structure using read_poscar() from utilities.
     structure = read_poscar(structure_path)
 
     repositioner = StructureRepositioner(structure)
-    repositioner.reposition_along_z(mode=args.mode, check_vacuum_layer_numbers=args.check_vacuum_layer_number)
+    repositioner.reposition_along_z(mode=args.mode)
 
-    # Save the modified structure to a file
     write(f'POSCAR_repositioned_{args.mode}', repositioner.structure, format='vasp')
 
 if __name__ == "__main__":
