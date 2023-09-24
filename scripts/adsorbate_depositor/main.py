7e1738ba2206d0b9a589061da22577996caa076b
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-09-24 20:17:41 +0800

    remove config loader, update docstring

--- scripts/adsorbate_depositor/main.py
+++ scripts/adsorbate_depositor/main.py
@@ -2,28 +2,39 @@
 # -*- coding: utf-8 -*-
 
 from pathlib import Path
+
 from src.configHandler import ConfigHandler
 from src.adsorbateGenerator import AdsorbateGenerator
 from src.siteGenerator import SiteGenerator
 from src.adsorbateDepositor import AdsorbateDepositor
 
-def load_configuration():
-    """Load or create configuration.
-
-    Returns:
-        dict: Loaded configuration or None if a template was copied.
+def main():
     """
+    Main entry point for the computational material science project. This function orchestrates the workflow for generating,
+    placing, and saving atomistic structures with adsorbates on substrate surfaces.
+
+    Workflow:
+    1. Checks for the existence of a configuration file and loads it if found. If not found, a template is copied.
+    2. Generates adsorption sites on the substrate surface using the SiteGenerator class.
+    3. Generates various adsorbate structures using the AdsorbateGenerator class.
+    4. Deposits the adsorbates onto the substrate surface at the generated sites using the AdsorbateDepositor class.
+    5. Writes the generated structures to output files.
+
+    Configuration file:
+    The program relies on a YAML-based configuration file for various settings. The ConfigHandler class is responsible for
+    managing this configuration.
+
+    Output:
+    Atomistic structures of substrate with adsorbates are saved in the specified output directory.
+
+    """
+    # Load or generate the configuration
     cfg_handler = ConfigHandler()
     if cfg_handler.check_config_exists():
-        return cfg_handler.load_config()
+        config = cfg_handler.load_config()
     else:
-        cfg_handler.copy_config_template(template_path=Path("database") / "config_template.yaml")
-        return None
-
-def main():
-    """Main entry point for the project."""
-    config = load_configuration()
-    if config is None:
+        template_path = Path("database") / "config_template.yaml"
+        cfg_handler.copy_config_template(template_path)
         print("No configuration loaded. Exiting.")
         return
 
@@ -61,8 +72,8 @@ def main():
     structures = structure_generator.deposit(
         auto_offset_along_z=config["deposit"]["auto_offset_along_z"],
         fix_substrate=config["deposit"]["fix_substrate"],
-        # target_vacuum_level=config["deposit"]["target_vacuum_level"],
-        # center_along_z=config["deposit"]["center_along_z"],
+        target_vacuum_level=config["deposit"]["target_vacuum_level"],
+        center_along_z=config["deposit"]["center_along_z"],
     )
 
     # Write generated models to file
