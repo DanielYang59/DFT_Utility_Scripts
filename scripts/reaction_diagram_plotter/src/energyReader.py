4bb74deeddb8ce4ff42ac209004afb060a05e740
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-10-09 15:48:46 +0800

    separate energy reader

--- scripts/reaction_diagram_plotter/src/energyReader.py
+++ scripts/reaction_diagram_plotter/src/energyReader.py
@@ -1,9 +1,45 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from pathlib import Path
+import pandas as pd
+
 class EnergyReader:
-    def  __init__(self) -> None:
-        pass
+    def  __init__(self, intermediate_energy_file: Path, species_energy_file: Path,) -> None:
+        """
+        TODO:
+
+        Args:
+            intermediate_energy_file (Path): Path to the intermediate energy CSV file.
+            species_energy_file (Path): Path to the species energy CSV file.
+            external_potential (Union[float, int], optional): External potential in volts. Defaults to 0.
+
+
+        """
+        # Import intermediate energies and species energies
+        self.intermediate_energies = self._import_energy_csv_file(intermediate_energy_file)
+        self.species_energies = self._import_energy_csv_file(species_energy_file)
+
+    def _import_energy_csv_file(self, file: Path, energy_name: str = "free_energy") -> Dict[str, float]:
+        """
+        Import and parse energy data from a CSV file into a dictionary.
+
+        Args:
+            file (Path): Path to the CSV file containing energy data.
+
+        Raises:
+            FileNotFoundError: If the CSV file is not found or has an illegal extension.
+
+        Returns:
+            Dict[str, float]: A dictionary where keys are column names and values are energy values.
+        """
+        # Check if the file exists and has a ".csv" extension
+        if not file.is_file() or file.suffix != ".csv":
+            raise FileNotFoundError(f"Energy CSV file '{file}' not found or illegal.")
+
+        # Import energy CSV file using pandas and convert to dictionary
+        energy_data = pd.read_csv(file)
+        return dict(zip(energy_data['name'], energy_data[energy_name]))
 
     def _read_molecule_energy(self, name: str) -> float:
         pass
@@ -13,3 +49,8 @@ class EnergyReader:
 
     def _read_intermediate_energy(self, name: str) -> float:
         pass
+
+# Test area
+if __name__ == "__main__":
+    intermediate_energy_file=Path("../example_usage/example_intermediate_energies.csv"),
+    species_energy_file=Path("../example_usage/example_species_energies.csv"),
--- scripts/reaction_diagram_plotter/src/reactionEnergyCalculator.py
+++ scripts/reaction_diagram_plotter/src/reactionEnergyCalculator.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from pathlib import Path
 from typing import Union, Dict
-import pandas as pd
-import warnings
 
 class ReactionEnergyCalculator:
     """
     TODO:
     """
 
-    def __init__(self, intermediate_energy_file: Path, species_energy_file: Path, external_potential: Union[float, int] = 0, pH: Union[float, int] = 7, verbose: bool = True) -> None:
+    def __init__(self, external_potential: Union[float, int] = 0, pH: Union[float, int] = 7, verbose: bool = True) -> None:
         """
-        Initialize the class instance with energy files, external potential, and pH.
+        Initialize the class instance with external potential and pH.
 
         Args:
-            intermediate_energy_file (Path): Path to the intermediate energy CSV file.
-            species_energy_file (Path): Path to the species energy CSV file.
             external_potential (Union[float, int], optional): External potential in volts. Defaults to 0.
             pH (Union[float, int], optional): pH value (0 to 14). Defaults to 7.
             verbose (bool, optional): global verbose level. Defaults to True.
@@ -27,10 +22,6 @@ class ReactionEnergyCalculator:
             ValueError: If pH is not within the range of 0 to 14.
             TypeError: If verbose is not boolean.
         """
-        # Import intermediate energies and species energies
-        self.intermediate_energies = self._import_energy_csv_file(intermediate_energy_file)
-        self.species_energies = self._import_energy_csv_file(species_energy_file)
-
         # Check and set external_potential
         if not isinstance(external_potential, (float, int)):
             raise TypeError("External potential must be a float or an integer.")
@@ -47,27 +38,6 @@ class ReactionEnergyCalculator:
         if not isinstance(verbose, bool):
             raise TypeError("Illegal data type for verbose.")
         self.verbose = verbose
-
-    def _import_energy_csv_file(self, file: Path, energy_name: str = "free_energy") -> Dict[str, float]:
-        """
-        Import and parse energy data from a CSV file into a dictionary.
-
-        Args:
-            file (Path): Path to the CSV file containing energy data.
-
-        Raises:
-            FileNotFoundError: If the CSV file is not found or has an illegal extension.
-
-        Returns:
-            Dict[str, float]: A dictionary where keys are column names and values are energy values.
-        """
-        # Check if the file exists and has a ".csv" extension
-        if not file.is_file() or file.suffix != ".csv":
-            raise FileNotFoundError(f"Energy CSV file '{file}' not found or illegal.")
-
-        # Import energy CSV file using pandas and convert to dictionary
-        energy_data = pd.read_csv(file)
-        return dict(zip(energy_data['name'], energy_data[energy_name]))
 
     def _calculate_half_reaction_energy(self, species: Dict[str, int]) -> float:
         """TODO:
@@ -116,20 +86,7 @@ class ReactionEnergyCalculator:
             pathway (dict): A dictionary containing 'reactants' and 'products' keys, each with their respective components.
 
         Returns:
-            tuple: A tuple containing the total energies of (products, reactants).
-
-        Calculates the energy change for the given reaction step by computing the energies of products and reactants separately.
-        The input 'pathway' dictionary should have 'reactants' and 'products' keys, where each key maps to a dictionary of
-        molecular components and their respective quantities.
-
-        Example:
-            For pathway input:
-            {
-                "reactants": {"A": 2, "B": 1},
-                "products": {"C": 1, "D": 1}
-            }
-
-            The returned tuple would be (energy_of_products, energy_of_reactants).
+            (float, float): The total energies of (products, reactants).
 
         """
         # Calculate energy for products
@@ -159,20 +116,19 @@ class ReactionEnergyCalculator:
         self.reaction_pathways = reaction_pathways
 
         # Calculate energy change for each reaction step
-        energy_changes = {}
+        energy_change = {}
         for index, pathway in self.reaction_pathways.items():
             # Calculate products and reactants energies
             products_energy, reactants_energy = self._calculate_energy_change_for_step(pathway)
 
             # Calculate energy change
-            energy_change = products_energy - reactants_energy
-            energy_changes[index] = energy_change
+            energy_change[index] = products_energy - reactants_energy
 
             # Verbose
             if self.verbose:
-                print(f"Step {index + 1}: product_energy {products_energy} eV, reactant_energy {reactants_energy} eV, energy_change {energy_change}")
+                print(f"Step {index + 1}: product_energy {products_energy} eV, reaction")
 
-        return energy_changes
+        return energy_change
 
 # Test area
 if __name__ == "__main__":
@@ -181,10 +137,7 @@ if __name__ == "__main__":
 
     # Initiate reaction energy calculator
     calculator = ReactionEnergyCalculator(
-        intermediate_energy_file=Path("../example_usage/example_intermediate_energies.csv"),
-        species_energy_file=Path("../example_usage/example_species_energies.csv"),
         external_potential=0
         )
 
-    # Calculate energy change
     calculator.calculate_energy_change(pathway)
