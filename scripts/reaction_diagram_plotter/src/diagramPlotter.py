78d660d60067e2ed601f96ec772d9eee26427271
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-11-22 16:40:21 +0800

    add total energy change calculate method

--- scripts/reaction_diagram_plotter/src/diagramPlotter.py
+++ scripts/reaction_diagram_plotter/src/diagramPlotter.py
@@ -19,9 +19,9 @@ class DiagramPlotter:
         self.energy_changes = [energy_changes[step] for step in sorted(energy_changes.keys())]
         self.absolute_energies = self._calculate_absolute_energies()
 
-        # # Calculate total energy change and equilibrium potential
-        # total_energy_change = self._calculate_total_energy_change()
-        # equilibrium_potential = self._calculate_equilibrium_potential()
+        # Calculate total energy change and equilibrium potential
+        self.total_energy_change = self._calculate_total_energy_change()
+        self.equilibrium_potential = self._calculate_equilibrium_potential()
 
     def _calculate_absolute_energies(self) -> List[float]:
         """
@@ -41,6 +41,21 @@ class DiagramPlotter:
         return abs_energies
 
     def _calculate_total_energy_change(self) -> float:
+        """
+        Calculate total energy change based on energy changes.
+
+        Returns:
+            float: total energy change in eV.
+        """
+        return sum(self.energy_changes)
+
+    def _calculate_equilibrium_potential(self) -> float:
+        """
+        Calculate equilibrium potential from total energy change and total electron transfer.
+
+        Returns:
+            float: equilibrium potential in V.
+        """
         pass
 
     def generate_plot(self, saveplot: Optional[bool] = False, path: Optional[Path] = None, show_plot: Optional[bool] = True) -> None:
