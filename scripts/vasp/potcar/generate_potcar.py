e6c34d1ccc82ee92e844798870a11b125e87c0b8
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-11-23 15:47:05 +0800

    add fetch recommended potential method

--- scripts/vasp/potcar/generate_potcar.py
+++ scripts/vasp/potcar/generate_potcar.py
@@ -4,7 +4,8 @@
 import os
 from pathlib import Path
 import warnings
-from typing import List
+from typing import List, Dict
+import yaml
 
 class PotcarGenerator:
     """
@@ -14,19 +15,68 @@ class PotcarGenerator:
         potcar_lib (Path): A Path object representing the directory where the POTCAR library resides.
 
     Methods:
+        _get_potcar_lib() -> Path:
+            Obtains the POTCAR library path from the environment variable.
         generate_potcar(elements: List[str], output_potcarfile: Path = Path("POTCAR")) -> None:
             Generates a POTCAR file based on a list of elements.
         get_elements_from_poscar(poscarfile: Path) -> List[str]:
             Retrieves the list of elements from a given POSCAR file.
-        get_potcar_lib() -> Path:
-            Obtains the POTCAR library path from the environment variable.
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         """
         Initialize the PotcarGenerator class.
         """
-        self.potcar_lib = self.get_potcar_library_path()
+        self.potcar_lib = self._get_potcar_library_path()
+
+    def _get_potcar_library_path(self) -> Path:
+        """
+        Get the path to the POTCAR library from the environment variable.
+
+        Returns:
+            Path: Path to the POTCAR library.
+
+        Raises:
+            EnvironmentError: If the POTCAR_LIBRARY_PATH environment variable is not set.
+            FileNotFoundError: If the path specified in the environment variable does not exist or is not a directory.
+        """
+        POTCAR_LIBRARY_PATH = os.environ.get("POTCAR_LIBRARY_PATH")
+        if POTCAR_LIBRARY_PATH is None:
+            raise EnvironmentError(
+                "The POTCAR_LIBRARY_PATH environment variable is not set. "
+                "Please set this variable to the path of your POTCAR library."
+            )
+
+        potcar_path = Path(POTCAR_LIBRARY_PATH)
+        if not potcar_path.is_dir():
+            raise FileNotFoundError(
+                f"The path specified in POTCAR_LIBRARY_PATH ({POTCAR_LIBRARY_PATH}) either does not exist "
+                "or is not a directory. Please check the path and try again."
+            )
+
+        return potcar_path
+
+    def _load_recommended_potentials(self, potential_file: Path = "recommended_paw_potentials.yaml") -> Dict[str, str]:
+        """
+        Load VASP recommended PAW potentials from a YAML file.
+
+        Parameters:
+            potential_file (Path, optional): Path to the YAML file containing recommended PAW potentials. Defaults to "recommended_paw_potentials.yaml".
+
+        Returns:
+            Dict[str, str]: A dictionary mapping element names to the recommended PAW potentials.
+
+        Raises:
+            FileNotFoundError: If the specified potential_file is not found.
+        """
+
+        if not potential_file.is_file():
+            raise FileNotFoundError(f"VASP recommended potential file {potential_file} not found.")
+
+        with open(potential_file) as f:
+            data = yaml.safe_load(f)
+
+        return data
 
     def get_elements_from_poscar(self, poscarfile: Path) -> list:
         """
@@ -54,34 +104,7 @@ class PotcarGenerator:
 
         return elements
 
-    def get_potcar_library_path(self) -> Path:
-        """
-        Get the path to the POTCAR library from the environment variable.
-
-        Returns:
-            Path: Path to the POTCAR library.
-
-        Raises:
-            EnvironmentError: If the POTCAR_LIBRARY_PATH environment variable is not set.
-            FileNotFoundError: If the path specified in the environment variable does not exist or is not a directory.
-        """
-        POTCAR_LIBRARY_PATH = os.environ.get("POTCAR_LIBRARY_PATH")
-        if POTCAR_LIBRARY_PATH is None:
-            raise EnvironmentError(
-                "The POTCAR_LIBRARY_PATH environment variable is not set. "
-                "Please set this variable to the path of your POTCAR library."
-            )
-
-        potcar_path = Path(POTCAR_LIBRARY_PATH)
-        if not potcar_path.is_dir():
-            raise FileNotFoundError(
-                f"The path specified in POTCAR_LIBRARY_PATH ({POTCAR_LIBRARY_PATH}) either does not exist "
-                "or is not a directory. Please check the path and try again."
-            )
-
-        return potcar_path
-
-    def generate_potcar(self, elements: List[str], output_potcarfile: Path = Path("POTCAR"), warn_element_num: int = 20, verbose: bool = True) -> None:
+    def generate_potcar(self, elements: List[str], output_potcarfile: Path = Path("POTCAR"), warn_element_num: int = 20, fetch_recommended: bool = False, verbose: bool = True) -> None:
         """
         Generate a POTCAR file based on a list of elements.
 
@@ -89,6 +112,7 @@ class PotcarGenerator:
             elements (list): A list containing the chemical symbols of the elements.
             output_potcarfile (Path): A Path object for the output POTCAR file. Defaults to 'POTCAR' in the current directory.
             warn_element_num (int, Optional): The number of element count threshold to generate "too many elements" warning. Defaults to 20.
+            fetch_recommended (bool, Optional): Fetch VASP recommended PAW potentials from "recommended_paw_potentials.yaml" file. Defaults to False.
             verbose (bool, Optional): Print list of elements. Defaults to True.
 
         Raises:
@@ -104,10 +128,18 @@ class PotcarGenerator:
             if element not in valid_elements:
                 raise ValueError(f"{element} is not a valid chemical element.")
 
+        # Load VASP recommended potentials
+        recommended_potentials = self._load_recommended_potentials()
+
         # Initialize an empty string to store POTCAR data
         potcar_data = ""
 
         for element in elements:
+            # Get VASP recommended potentials if required
+            if fetch_recommended and element in recommended_potentials.keys():
+                element = recommended_potentials[element]
+                warnings.warn(f"VASP recommended PAW potential {element} loaded.")
+
             element_potcar_path = self.potcar_lib / element / "POTCAR"
 
             # Check if POTCAR file for the element exists
@@ -121,7 +153,7 @@ class PotcarGenerator:
         # Print element list
         if verbose:
             print(f"POTCAR for elements: '{','.join(elements)}' generated.")
-        
+
         # Write the combined POTCAR data to the output path
         with output_potcarfile.open("w", encoding="utf-8") as f:
             f.write(potcar_data)
