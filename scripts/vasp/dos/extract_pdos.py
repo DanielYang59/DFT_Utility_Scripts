4a767e33d76a5226acac937ffc06963567d398e7
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-11-23 18:26:06 +0800

    update docstring

--- scripts/vasp/dos/extract_pdos.py
+++ scripts/vasp/dos/extract_pdos.py
@@ -7,7 +7,25 @@ from src.userConfigParser import UserConfigParser
 from src.vasprunXmlReader import VasprunXmlReader
 from src.outputPdosGenerator import OutputPdosGenerator
 
-def main(configfile=Path("PDOSIN")):
+def main(configfile=Path("PDOSIN")) -> None:
+    """
+    Main function for extracting Partial Density of States (PDOS) data from vasprun.xml file.
+
+    Parameters:
+        configfile (Path, optional): Path to the configuration file (PDOSIN). Defaults to "PDOSIN" in the current working directory.
+
+    Returns:
+        None
+
+    This function reads the configuration file, parses the requested curves, imports the vasprun.xml file,
+    reads the Fermi level and ISPIN tag, and fetches PDOS data for each requested curve.
+    The fetched PDOS data, along with energy data, is then written to the output file "PDOS.dat" in the current working directory.
+    If the specified configuration file does not exist, a config template is generated.
+
+    Note: The PDOSIN configuration file specifies the curves for which PDOS data should be generated.
+    The vasprun.xml file is assumed to be present in the current working directory.
+
+    """
     # Get current working directory
     cwd = Path.cwd()
 
@@ -26,7 +44,7 @@ def main(configfile=Path("PDOSIN")):
     vasprunxml_reader = VasprunXmlReader(vasprunXmlFile=cwd / "vasprun.xml")
 
     ## Read fermi level and ISPIN tag from vasprun.xml
-    ispin = vasprunxml_reader.read_incar_tag(tag="ISPIN")
+    ispin = vasprunxml_reader.read_incar_tag(tag="ISPIN")  # TODO: need to consider ispin = 1
     fermi_level = vasprunxml_reader.read_fermi_level()
 
     # For each curve required, fetch PDOS data
