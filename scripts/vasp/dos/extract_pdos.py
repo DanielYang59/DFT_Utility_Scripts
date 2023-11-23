17c7829e1f322918aa3f81e7e28b671b934f7e4d
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-11-23 18:24:18 +0800

    update code skeleton

--- scripts/vasp/dos/extract_pdos.py
+++ scripts/vasp/dos/extract_pdos.py
@@ -1,56 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
-""" Brief Intro:  # DEBUG: remove after refactoring
-Generate PDOS data from "vasprun.xml" file, by YangHy.
-If you find this script helpful or have any issue/advice to report,
-you could kindly reach out to me at yanghaoyu97@outlook.com.
-Deeply appreciate the help from Mr. WANG Qiankun and the advice from Dr. ZHAO Juanli.
-"""
-
-
 from pathlib import Path
 
 from src.userConfigParser import UserConfigParser
 from src.vasprunXmlReader import VasprunXmlReader
 from src.outputPdosGenerator import OutputPdosGenerator
 
+def main(configfile=Path("PDOSIN")):
+    # Get current working directory
+    cwd = Path.cwd()
 
-def main(configfile=Path("PDOSIN")):  # NOTE: rewrite to current working dir
-    # Read config file 
-    config_parser = UserConfigParser(configfile=configfile)
-    
+    # Read config file
+    config_parser = UserConfigParser(configfile=cwd / configfile)
+
     if not configfile.is_file():  # or generate config template
         config_parser.generate_config_template()
-    
+
     config_parser.read_config()
-    
+
     # Get requested curve list
     requested_curves = config_parser.parse()
 
-
     # Import vasprun.xml file
-    vasprunxml_reader = VasprunXmlReader(vasprunXmlFile="vasprun.xml")  # NOTE: rewrite to current working dir
-    
-    ## Read fermi level and ISPIN tag
-    # TODO: read fermi level
+    vasprunxml_reader = VasprunXmlReader(vasprunXmlFile=cwd / "vasprun.xml")
+
+    ## Read fermi level and ISPIN tag from vasprun.xml
     ispin = vasprunxml_reader.read_incar_tag(tag="ISPIN")
-    
-    
+    fermi_level = vasprunxml_reader.read_fermi_level()
+
     # For each curve required, fetch PDOS data
     fetched_pdos_data = [
         vasprunxml_reader.read_curve(curve_info=requested_curve)
         for requested_curve in requested_curves
         ]
-    
-    
-    # TODO: remember to insert energy data
-    
-    
-    # Output fetched PDOS data
-    output_generator = OutputPdosGenerator(fetched_pdos_data)
 
+    # Fetch energy data
+    energies = vasprunxml_reader.read_energies()
+    energies -= fermi_level
+
+    # Output fetched PDOS data
+    output_generator = OutputPdosGenerator(fetched_pdos_data, energies)
+    output_generator.write(cwd / "PDOS.dat")
 
 if __name__ == "__main__":
     main()
