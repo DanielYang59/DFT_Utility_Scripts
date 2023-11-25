8aea5711b3f55f42c80709d77435ccdf12550d38
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-11-25 20:13:46 +0800

    temp save

--- scripts/vasp/dos/src/write_output_pdos.py
+++ scripts/vasp/dos/src/write_output_pdos.py
@@ -17,25 +17,23 @@ def write_output_pdos(pdos_data: list, fermi_level: float, filename: Path) -> No
     - FileExistsError: If the output file already exists.
 
     """
-    # Check output file name
-    if filename.is_file():
-        raise FileExistsError(f"Error: File '{filename}' already exists. Exiting to avoid overwriting.")
+    # Write header
+    header = ["Curve", "Energy", "PDOS_Spin_Up", "PDOS_Spin_Down"]
 
-    # Write each curve's header to file
+    # Write data for each curve, one column at a time
     with open(filename, mode='w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
-
-        header = ["Curve", "Energy", "PDOS_Spin_Up", "PDOS_Spin_Down"]
         csv_writer.writerow(header)
 
-        # Write data for each curve
-        for index, curve in enumerate(pdos_data):
-            # Unpack (energy_array, pdos_spin_up, pdos_spin_down) tuple
-            energy_array, pdos_spin_up, pdos_spin_down = curve
+        # Iterate through each energy level for all curves before moving to the next column
+        for energy_level in zip(*[curve[0] for curve in pdos_data]):
+            for index, curve in enumerate(pdos_data):
+                # Unpack (energy_array, pdos_spin_up, pdos_spin_down) tuple
+                energy_array, pdos_spin_up, pdos_spin_down = curve
 
-            # Reference energy to fermi level
-            energy_array -= fermi_level
+                # Reference energy to fermi level
+                energy_level_ref = energy_level[index] - fermi_level
 
-            # Write data for each energy level
-            for energy, spin_up, spin_down in zip(energy_array, pdos_spin_up, pdos_spin_down):
-                csv_writer.writerow([f"Curve_{index + 1}", energy, spin_up, spin_down])
+                # Write data for the current energy level
+                csv_writer.writerow([f"Curve_{index + 1}", energy_level_ref, pdos_spin_up[index], pdos_spin_down[index]])
+
