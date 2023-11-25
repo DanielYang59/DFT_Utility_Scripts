f0f98ac63384367a4253cf619d7350841b0ddbb7
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-11-25 20:07:41 +0800

    fix data writing

--- scripts/vasp/dos/src/write_output_pdos.py
+++ scripts/vasp/dos/src/write_output_pdos.py
@@ -21,10 +21,14 @@ def write_output_pdos(pdos_data: list, fermi_level: float, filename: Path) -> No
     if filename.is_file():
         raise FileExistsError(f"Error: File '{filename}' already exists. Exiting to avoid overwriting.")
 
-    # Write each curve to file
+    # Write each curve's header to file
     with open(filename, mode='w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
 
+        header = ["Curve", "Energy", "PDOS_Spin_Up", "PDOS_Spin_Down"]
+        csv_writer.writerow(header)
+
+        # Write data for each curve
         for index, curve in enumerate(pdos_data):
             # Unpack (energy_array, pdos_spin_up, pdos_spin_down) tuple
             energy_array, pdos_spin_up, pdos_spin_down = curve
@@ -32,16 +36,6 @@ def write_output_pdos(pdos_data: list, fermi_level: float, filename: Path) -> No
             # Reference energy to fermi level
             energy_array -= fermi_level
 
-            # Write separator column (curve_index)
-            separator_column = [f"Curve {index}"] * len(energy_array)
-            csv_writer.writerow(separator_column)
-
-            # Write energy_array, pdos_spin_up, pdos_spin_down to the CSV file
-            header = ["Energy", "PDOS_Spin_Up"]
-            if pdos_spin_down is not None:
-                header.append("PDOS_Spin_Down")
-
-            csv_writer.writerow(header)
-
-            rows = zip(energy_array, pdos_spin_up, pdos_spin_down) if pdos_spin_down is not None else zip(energy_array, pdos_spin_up)
-            csv_writer.writerows(rows)
+            # Write data for each energy level
+            for energy, spin_up, spin_down in zip(energy_array, pdos_spin_up, pdos_spin_down):
+                csv_writer.writerow([f"Curve_{index + 1}", energy, spin_up, spin_down])
