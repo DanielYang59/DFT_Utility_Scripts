4727f863e9d800e02437c715588c20f22c6f0ab1
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-11-25 19:40:20 +0800

    rename output generator

---
+++ scripts/vasp/dos/src/write_output_pdos.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+import numpy as np
+
+def write_output_pdos(energy_array: np.ndarray, spin_up_pdos: np.ndarray, spin_down_pdos: np.ndarray, filename: Path):
+    pass
