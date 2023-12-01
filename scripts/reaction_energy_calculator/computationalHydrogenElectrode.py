4bf02865f4420039cb2fc24165ad43bdb54f45a4
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-12-01 10:07:06 +0800

    add CHE initiator

---
+++ scripts/reaction_energy_calculator/computationalHydrogenElectrode.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from lib.reactionStep import ReactionStep
+
+class ComputationalHydrogenElectrode(ReactionStep):
+    """
+    Class representing a computational hydrogen electrode in a chemical reaction.
+
+    Additional Attributes:
+        additional_attribute (str): An additional attribute for the computational hydrogen electrode.
+
+    Additional Methods:
+        additional_method(): An additional method for the computational hydrogen electrode.
+    """
+
+    def __init__(self, pH: float, external_potential: float) -> float:
+        """
+        Initialize a ComputationalHydrogenElectrode instance.
+
+        Args:
+            deltaG0 (float): Standard Gibbs free energy change for the reaction.
+            temperature (float): Temperature of the reaction.
+            pH (float): pH of the reaction.
+            external_potential (float): External potential applied to the reaction.
+            additional_attribute (str): An additional attribute for the computational hydrogen electrode.
+        """
+        # Initialize CHE at standard free energy change zero and 298.15 K
+        super().__init__(0, 298.15, pH, external_potential)
+
+        # Set species
+        self.set_reactants({"H+":1, "e-":1})
+        self.set_products({"H2_g":0.5})
+
+        # Calculate free energy change
+        self.calculate_free_energy_change()
+
+        # Calculate pH and external potential corrections
+        pH_correction = self.calculate_pH_correction()
+        external_potential_correction = self.calculate_external_potential_correction()
+
+        return self.free_energy_change + pH_correction + external_potential_correction
