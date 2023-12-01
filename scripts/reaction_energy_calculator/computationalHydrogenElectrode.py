b38c7a2a131ef7c7d183d0932d6031736a42daa0
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-12-01 10:29:36 +0800

    add hydroxide energy calculation

--- scripts/reaction_energy_calculator/computationalHydrogenElectrode.py
+++ scripts/reaction_energy_calculator/computationalHydrogenElectrode.py
@@ -14,7 +14,7 @@ class ComputationalHydrogenElectrode(ReactionStep):
         additional_method(): An additional method for the computational hydrogen electrode.
     """
 
-    def __init__(self, pH: float, external_potential: float) -> None:
+    def __init__(self, pH: float, external_potential: float, gaseous_hydrogen_free_energy: float, liquid_water_free_energy: float = None) -> None:
         """
         Initialize a ComputationalHydrogenElectrode instance.
 
@@ -28,18 +28,13 @@ class ComputationalHydrogenElectrode(ReactionStep):
         # Initialize CHE at standard free energy change zero and 298.15 K
         super().__init__(0, 298.15, pH, external_potential)
 
-        # Set species
-        self.set_reactants({"H+":1, "e-":1})
-        self.set_products({"H2_g":0.5})
+        # Set species and species energies
+        assert gaseous_hydrogen_free_energy < 0
+        self.gaseous_hydrogen_free_energy = gaseous_hydrogen_free_energy
+        self.liquid_water_free_energy = liquid_water_free_energy
+        self.set_reactants(reactants={"H+":1, "e-":1}, reactant_energies={"H+":0, "e-":0})
+        self.set_products(products={"H2_g":0.5}, product_energies={"H2_g":gaseous_hydrogen_free_energy})
 
-    def calculate_proton_free_energy(self) -> float:
-        """
-        Calculate the free energy change for the computational hydrogen electrode
-        including pH and external potential corrections.
-
-        Returns:
-            float: The calculated proton free energy with pH and external potential corrections.
-        """
         # Calculate free energy change
         self.calculate_free_energy_change()
 
@@ -47,7 +42,33 @@ class ComputationalHydrogenElectrode(ReactionStep):
         pH_correction = self.calculate_pH_correction()
         external_potential_correction = self.calculate_external_potential_correction()
 
-        return self.free_energy_change + pH_correction + external_potential_correction
+        # Add corrections to free energy change
+        self.free_energy_change = self.free_energy_change + pH_correction + external_potential_correction
 
-    def calculate_hydroxide_free_energy(self) -> float:
-        pass
+    def calculate_proton_free_energy(self) -> float:
+        """
+        Calculate the free energy of proton-electron pair
+        including pH and external potential corrections.
+
+        Note:
+            From reaction (H+ + e-) --> 0.5* H2_g.
+
+        Returns:
+            float: The calculated proton-electron pair free energy with pH and external potential corrections.
+        """
+        return 0.5 * self.gaseous_hydrogen_free_energy - self.free_energy_change
+
+    def calculate_hydroxide_free_energy(self, water_association_free_energy: float = -0.828) -> float:
+        """
+        Calculate the free energy of hydroxide-electron pair
+        including pH and external potential corrections.
+
+        Note:
+            From reaction (H+ + e-) + (OH- - e-) --> H2O_l.
+
+        Returns:
+            float: The calculated hydroxide-electron pair free energy with pH and external potential corrections.
+        """
+        assert water_association_free_energy < 0
+
+        return self.liquid_water_free_energy - self.calculate_proton_free_energy - water_association_free_energy
