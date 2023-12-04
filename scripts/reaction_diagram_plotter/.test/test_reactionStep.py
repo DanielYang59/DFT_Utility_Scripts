8d3580bc3656d0a088a89bc4e4759ef1c3153434
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-12-04 10:13:09 +0800

    improve format

--- scripts/reaction_diagram_plotter/.test/test_reactionStep.py
+++ scripts/reaction_diagram_plotter/.test/test_reactionStep.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Test objectives: test the results of reactionSteps and compare it to manual calculations.
+Test objectives: Test the results of reactionSteps and compare them to manual calculations.
 
 Target Reaction One: H+ + e- --> 0.5 * H2_g for the following ΔGs:
     a. pH = 0, U = 0 (expect 0)
@@ -13,17 +13,16 @@ Target Reaction One: H+ + e- --> 0.5 * H2_g for the following ΔGs:
 
 Target Reaction Two: 0.5 * H2_g --> H+ + e- for the following ΔGs:
     (Expect reverses of Target One)
-    a. pH = 0, U = 0
-    b. pH = 7, U = 0
-    d. pH = 0, U = 1
-    e. pH = 0, U = -1
-    f. pH = 7, U = 1
+    a'. pH = 0, U = 0
+    b'. pH = 7, U = 0
+    d'. pH = 0, U = 1
+    e'. pH = 0, U = -1
+    f'. pH = 7, U = 1
 
 Target Reaction Three: H2O_l --> 0.5 * H2_g + OH- - e- for the following ΔGs:
     a. pH = 14, U = 0 (expect X)
     b. pH = 7, U = 0 (expect 0)
     c. pH = 14, U = 1 (expect 0)
-
 """
 
 from ..src.reactionStep import ReactionStep
