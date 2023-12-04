eacd441e61507117e53e5faa04f3ee4fefae99d5
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-12-04 13:07:11 +0800

    fix import

--- scripts/reaction_diagram_plotter/src/computationalHydrogenElectrode.py
+++ scripts/reaction_diagram_plotter/src/computationalHydrogenElectrode.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from .reactionStep import ReactionStep
+from reactionStep import ReactionStep
 
 class ComputationalHydrogenElectrode(ReactionStep):
     """
