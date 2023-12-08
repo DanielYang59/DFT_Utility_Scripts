6b0e5ac4feeb1476669fcba7aba09e9b3a52ad8b
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-12-08 19:26:08 +0800

    simplify return

--- scripts/vasp/poscar/coordinate_system_transfer.py
+++ scripts/vasp/poscar/coordinate_system_transfer.py
@@ -18,12 +18,10 @@ def cartesian_to_direct(atoms: Atoms) -> Atoms:
     scaled_positions = atoms.get_scaled_positions()
 
     # Create a new Atoms object with the scaled positions
-    new_atoms = Atoms(symbols=atoms.get_chemical_symbols(),
+    return Atoms(symbols=atoms.get_chemical_symbols(),
                       scaled_positions=scaled_positions,
                       cell=atoms.get_cell(),
                       pbc=atoms.get_pbc())
-
-    return new_atoms
 
 def direct_to_cartesian(atoms: Atoms) -> Atoms:
     """
@@ -39,12 +37,10 @@ def direct_to_cartesian(atoms: Atoms) -> Atoms:
     positions = atoms.get_positions()
 
     # Create a new Atoms object with the Cartesian positions
-    new_atoms = Atoms(symbols=atoms.get_chemical_symbols(),
+    return Atoms(symbols=atoms.get_chemical_symbols(),
                       positions=positions,
                       cell=atoms.get_cell(),
                       pbc=atoms.get_pbc())
-
-    return new_atoms
 
 def main():
     """
