854934e227b3275f97d08bcfd2ed27423f3a05d2
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-12-13 11:08:07 +0800

    improve incar test script

--- scripts/vasp/incar/.tests/read_INCAR/test_read_incar.py
+++ scripts/vasp/incar/.tests/read_INCAR/test_read_incar.py
@@ -10,7 +10,12 @@ sys.path.append(str(src_path))
 from vasp_incar import VaspIncar
 
 if __name__ == "__main__":
+    # Test reading legal INCAR
     incar_handler = VaspIncar(Path("./INCAR_legal"))
     print(incar_handler.incar_data)
     print(incar_handler.read_tag("ALGO"))
     print(incar_handler.read_tag("ISIF"))  # non-existing tag
+
+    # Test reading illegal INCAR
+    incar_handler = VaspIncar(Path("./INCAR_illegal"))
+    print(incar_handler.incar_data)
