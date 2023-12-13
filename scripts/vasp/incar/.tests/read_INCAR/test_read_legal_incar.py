178bd7342331cfe4e00d200dbfc271fe014e4e09
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-12-13 11:05:28 +0800

    finish legal incar read test

---
+++ scripts/vasp/incar/.tests/read_INCAR/test_read_legal_incar.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+from pathlib import Path
+
+src_path = Path(__file__).resolve().parents[2] / 'src'
+sys.path.append(str(src_path))
+
+from vasp_incar import VaspIncar
+
+if __name__ == "__main__":
+    incar_handler = VaspIncar(Path("./INCAR_legal"))
+    print(incar_handler.incar_data)
+    print(incar_handler.read_tag("ALGO"))
+    print(incar_handler.read_tag("ISIF"))  # non-existing tag
