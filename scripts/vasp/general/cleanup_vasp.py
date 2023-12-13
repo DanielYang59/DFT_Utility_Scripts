fe202e763fb80023ce4355406139d4aa65e102e1
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-12-13 13:28:01 +0800

    clean up script

--- scripts/vasp/general/cleanup_vasp.py
+++ scripts/vasp/general/cleanup_vasp.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# TODO: this script would need double check (why essential_files is unused)
-
-import os
 from pathlib import Path
+import shutil
 
-def clean_vasp_files(directory: Path, job_script: str = "script.sh", verbose: str = "silent"):
+def clean_vasp_files(directory: Path, job_script: str = "script.sh", verbose: bool = True):
     """
     Cleans up VASP output files, keeping only the essential ones.
     Moves other specified VASP output files to a backup directory,
@@ -15,30 +13,41 @@ def clean_vasp_files(directory: Path, job_script: str = "script.sh", verbose: st
     Args:
         directory (Path): The directory containing VASP files.
         job_script (str): The name of the job submit script.
-        verbose (str): Level of verbosity ("silent", "verbose").
+        verbose (bool): verbosity. Defaults to True.
     """
+    # Define essential and unnecessary files
     essential_files = ["INCAR", "POSCAR", "POTCAR", "KPOINTS", job_script]
-    vasp_output_files = [
+
+    files_to_remove = [
         "OUTCAR", "OSZICAR", "WAVECAR", "CHGCAR", "EIGENVAL", "vasprun.xml",
         "XDATCAR", "CONTCAR", "IBZKPT", "DOSCAR", "PCDAT", "REPORT"
     ]
 
+    # Create a new backup directory
     backup_index = 1
-
     while True:
-        backup_dir = directory / f"output_backup_{backup_index}"
+        backup_dir = directory / f"vasp_backup_{backup_index}"
         if not backup_dir.exists():
             backup_dir.mkdir()
             break
         backup_index += 1
 
+    # Move VASP output files to the backup directory
     for file_path in directory.iterdir():
-        if file_path.is_file() and file_path.name in vasp_output_files:
-            new_path = backup_dir / file_path.name
-            file_path.rename(new_path)
-            if verbose == "verbose":
-                print(f"Moved {file_path.name} to {backup_dir}")
+        if file_path.is_file():
+            if file_path.name in files_to_remove:
+                file_path.unlink(missing_ok=True)  # Remove unnecessary VASP output files
+
+            elif file_path.name in essential_files:
+                shutil.copy2(file_path, backup_dir / file_path.name)  # Copy essential files to the backup directory
+
+            else:
+                pass  # ignore other files
+
+    # Verbose
+    if verbose:
+        print(f"VASP dir {directory} cleaned up.")
 
 if __name__ == "__main__":
-    current_directory = Path(os.getcwd())
-    clean_vasp_files(current_directory)
+    # Clean up current working directory for VASP
+    clean_vasp_files(Path.cwd())
