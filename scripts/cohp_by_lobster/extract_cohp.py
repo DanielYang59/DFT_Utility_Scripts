8b486929b7106a21197faf695ae6483809b60894
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-12-15 10:19:03 +0800

    add load cohp method

--- scripts/cohp_by_lobster/extract_cohp.py
+++ scripts/cohp_by_lobster/extract_cohp.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 
-# Check Pymatgen: https://pymatgen.org/pymatgen.io.lobster.html
-# reference: https://gist.github.com/lan496/ee0bd7a52df99029ac0aacbe69f2bf57
+# https://pymatgen.org/pymatgen.io.lobster.html
+# https://pymatgen.org/pymatgen.electronic_structure.html
+
+# Tutorial: https://matgenb.materialsvirtuallab.org/2019/01/11/How-to-plot-and-evaluate-output-files-from-Lobster.html
 
 from pathlib import Path
 from pymatgen.electronic_structure.cohp import CompleteCohp
@@ -11,17 +13,47 @@ from pymatgen.electronic_structure.plotter import CohpPlotter
 
 # NOTE: energy from cohp should already by referenced to fermi level
 
+# WARNING: labels in "COHP.lobtser" and "ICOHP.lobster" might be different according to: https://matgenb.materialsvirtuallab.org/2019/01/11/How-to-plot-and-evaluate-output-files-from-Lobster.html
+
 
 class cohpExtractor:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, cohp_dir: Path = Path.cwd()) -> None:
+        # Load COHP data
+        self._load_cohp_data(working_dir=cohp_dir)
 
 
-    def load_cohp_data(self) -> None:
-        pass
+    def _load_cohp_data(self, working_dir: Path) -> None:
+        """
+        Load COHP (Crystal Orbital Hamilton Population) data from a Lobster calculation.
+
+        This method reads COHPCAR.lobster and POSCAR files from the specified directory
+        and initializes the `complete_cohp` attribute with the loaded data.
+
+        Parameters:
+            working_dir (Path): The path to the directory containing COHPCAR.lobster and POSCAR files.
+
+        Raises:
+            FileNotFoundError: If either COHPCAR.lobster or POSCAR is not found in the specified directory.
+
+        Notes:
+            Reference: https://pymatgen.org/pymatgen.electronic_structure.html
+
+        Returns:
+            None
+        """
+        # Check core files
+        if not (working_dir / 'COHPCAR.lobster').is_file():
+            raise FileNotFoundError(f"COHPCAR.lobster not found in {working_dir}.")
+        if not (working_dir / 'POSCAR').is_file():
+            raise FileNotFoundError(f"POSCAR not found in {working_dir}.")
+
+        # Load complete COHP
+        self.complete_cohp = CompleteCohp.from_file(fmt='LOBSTER', filename=working_dir / 'COHPCAR.lobster', structure_file= working_dir / 'POSCAR')
 
 
     def get_interation_pair(self) -> None:  # NOTE: double check method name and return hint
+        # This seems to be defined as "label" in the "get_cohp_by_label" method
+        # by pymatgen: https://pymatgen.org/pymatgen.electronic_structure.html
         pass
 
 
