d1a8cbf03262042dc934722d15f217ec65011b19
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-12-15 18:01:03 +0800

    refine language

--- scripts/reaction_diagram_plotter/example_usage/runexample.sh
+++ scripts/reaction_diagram_plotter/example_usage/runexample.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# -*- coding: utf-8 -*-
 
-# Define external potential (V) and pH
+# Define external potential and pH
 EXTERNAL_POTENTIAL=0
 PH=14
 
-# Call the plotter
-
+# Call the calculator
 python ../main.py -U $EXTERNAL_POTENTIAL -ph $PH
