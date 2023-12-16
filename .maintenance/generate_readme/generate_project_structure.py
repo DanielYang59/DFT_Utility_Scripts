4d616ce9e0641913e613e03eeb51da1f66a9bb9c
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-12-16 11:32:43 +0800

    update skeleton

--- .maintenance/generate_readme/generate_project_structure.py
+++ .maintenance/generate_readme/generate_project_structure.py
@@ -1,26 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import asciitree
-from pathlib import Path
 
+import json
+from pathlib import Path
+from asciitree import LeftAligned
+from collections import OrderedDict as OD
 
 class ProjectStructureGenerator:
-    def __init__(self) -> None:
+    def __init__(self, structure_recipe: Path) -> None:
+        # Load structure recipe file
+        assert structure_recipe.is_file()
+        with open(self.structure_recipe, 'r') as file:
+            self.structure_data = json.load(file)
+
+    def generate_structure_tree(self) -> str:
         pass
 
 
+    def create_readme_part(self, structure_tree: str, filename: Path) -> None:
+
+        # Write project structure to .md file
+        with open(filename, 'w') as file:
+            file.write("# Project Structure\n")
+            file.write(structure_tree)
+
+
 def generate_project_structure():
-    # Initialize the generator
-    generator = ProjectStructureGenerator()
-
-    # Read the "project structure" recipe file
+    # Initialize the generator and read recipe file
+    generator = ProjectStructureGenerator(structure_recipe=Path("./project_structure_recipe.json"))
 
 
-    # Generate structure tree
+    # Generate structure tree string
+    structure_tree = generator.generate_structure_tree()
 
 
-    # Save structure tree to file
+    # Create README part file
+    generator.create_readme_part(structure_tree, filename=Path("./readme_parts/project_structure.md"))
 
 
 if __name__ == "__main__":
