7685372b2a98e40d0382823d54a265a52af4c155
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-12-19 11:09:42 +0800

    update skeleton

--- scripts/vasp/general/check_vasp_status.py
+++ scripts/vasp/general/check_vasp_status.py
@@ -4,25 +4,42 @@
 
 from pathlib import Path
 
+from pymatgen.io.vasp import Vasprun
+
 from .is_vasp_dir import is_vasp_dir
 
 
-class vaspStatusChecker:
+class VaspStatusChecker:
     def __init__(self, working_dir: Path) -> None:
-        assert working_dir.is_dir()
+        # Instead of raising an error, return None so that the workflow would not be interrupted
+        if not working_dir.is_dir():
+            self.job_type = None
+            self.reason = "Directory not exists."
+
+        elif not is_vasp_dir(working_dir):
+            self.job_type = None
+            self.reason = "Not a legal VASP directory."
+
+        elif not (working_dir / "vasprun.xml").is_file():
+            self.job_type = None
+            self.reason = "vasprun.xml not found."
+
+        # Import vasprun.xml file
         self.working_dir = working_dir
+        self.vasprun = Vasprun(working_dir / "vasprun.xml")
 
 
-    def read_job_type(self) -> None:
+    def check_job_type(self) -> str:
+        # Pymatgen doesn't seem to have a proper method for this......yet.
         pass
 
 
     def check_convergence(self) -> bool:
-        pass
+        return self.vasprun.converged
 
 
-    def print_out_results(self) -> None:
-        pass
+    def get_energy(self) -> float:
+        return self.vasprun.final_energy
 
 
 def check_vasp_status():
