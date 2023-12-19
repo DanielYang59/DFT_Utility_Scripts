4d1c12aa50d9c97e5ba07553a6b0207757b2cbfe
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-12-19 12:03:00 +0800

    add check_job_type method

--- scripts/vasp/general/check_vasp_status.py
+++ scripts/vasp/general/check_vasp_status.py
@@ -2,14 +2,19 @@
 # -*- coding: utf-8 -*-
 
 
+# TODO: default INCAR tags are not handled
+
+
 from pathlib import Path
 
-from pymatgen.io.vasp import Vasprun
+from pymatgen.io.vasp import Vasprun, Incar
 
 from .is_vasp_dir import is_vasp_dir
 
 
 class VaspStatusChecker:
+    # TODO:
+
     def __init__(self, working_dir: Path) -> None:
         """
         Initialize the VaspStatusChecker.
@@ -20,19 +25,20 @@ class VaspStatusChecker:
         # Instead of raising an error, return None so that the workflow would not be interrupted
         if not working_dir.is_dir():
             self.job_type = None
-            self.reason = "Directory not exists."
+            self.comment = "Directory not exists."
 
         elif not is_vasp_dir(working_dir):
             self.job_type = None
-            self.reason = "Not a legal VASP directory."
+            self.comment = "Not a legal VASP directory."
 
         elif not (working_dir / "vasprun.xml").is_file():
             self.job_type = None
-            self.reason = "vasprun.xml not found."
+            self.comment = "vasprun.xml not found."
 
         # Import vasprun.xml file
         self.working_dir = working_dir
         self.vasprun = Vasprun(working_dir / "vasprun.xml")
+        self.incar = Incar().from_file(working_dir / "INCAR")
 
 
     def check_job_type(self) -> str:
@@ -41,9 +47,55 @@ class VaspStatusChecker:
 
         Returns:
             str: The type of the VASP calculation job.
+
+        Notes:
+            Pymatgen doesn't seem to have a proper method for this... yet.
+
+        The job types are determined based on specific INCAR parameters. The recognized job types
+        include 'cell_opt' for cell optimization, 'ion_opt' for ionic optimization, 'scf' for
+        self-consistent field calculations, and 'unknown' for jobs with an unknown type.
+
+        - Job type 'cell_opt':
+          Conditions: ISIF = 3; IBRION = 2; NSW > 0
+          Reference: https://www.vasp.at/tutorials/latest/bulk/part2/#bulk-e05
+
+        - Job type 'ion_opt':
+          Conditions: ISIF = 2; IBRION = 2; NSW > 0
+          Reference: https://www.vasp.at/tutorials/latest/molecules/part3/
+
+        - Job type 'scf':
+          Conditions: IBRION = -1 or NSW = 0
+
+        - Job type 'unknown':
+          Conditions: None of the above conditions are met.
         """
-        # Pymatgen doesn't seem to have a proper method for this......yet.
-        pass
+        # Job type: Cell optimization
+        # ISIF = 3; IBRION = 2; NSW > 0
+        # Reference: https://www.vasp.at/tutorials/latest/bulk/part2/#bulk-e05
+        if self.incar["ISIF"] == 3 and self.incar["IBRION"] == 2 and self.incar["NSW"] > 0:
+            self.job_type = "cell_opt"
+            self.comment = "cell optimization"
+
+
+        # Job type: Ionic optimization
+        # ISIF = 2; IBRION = 2; NSW > 0
+        # Reference: https://www.vasp.at/tutorials/latest/molecules/part3/
+        elif self.incar["ISIF"] == 2 and self.incar["IBRION"] == 2 and self.incar["NSW"] > 0:
+            self.job_type = "ion_opt"
+            self.comment = "Ion optimization"
+
+
+        # Job type: SCF
+        # IBRION = -1; NSW = 0
+        elif self.incar["IBRION"] == -1 or self.incar["NSW"] == 0:
+            self.job_type = "scf"
+            self.comment = "self-consistent field"
+
+
+        # Job type: Unknown
+        else:
+            self.job_type = "unknown"
+            self.comment = "Unknown job type"
 
 
     def check_convergence(self) -> bool:
