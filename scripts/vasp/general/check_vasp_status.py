64902a0d1f05094d62873d822202cd2ed5a2db1a
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2023-12-19 11:11:31 +0800

    update docstring

--- scripts/vasp/general/check_vasp_status.py
+++ scripts/vasp/general/check_vasp_status.py
@@ -11,6 +11,12 @@ from .is_vasp_dir import is_vasp_dir
 
 class VaspStatusChecker:
     def __init__(self, working_dir: Path) -> None:
+        """
+        Initialize the VaspStatusChecker.
+
+        Args:
+            working_dir (Path): The path to the VASP calculation directory.
+        """
         # Instead of raising an error, return None so that the workflow would not be interrupted
         if not working_dir.is_dir():
             self.job_type = None
@@ -30,15 +36,33 @@ class VaspStatusChecker:
 
 
     def check_job_type(self) -> str:
+        """
+        Check and return the type of the VASP calculation job.
+
+        Returns:
+            str: The type of the VASP calculation job.
+        """
         # Pymatgen doesn't seem to have a proper method for this......yet.
         pass
 
 
     def check_convergence(self) -> bool:
+        """
+        Check and return the convergence status (both ionic and electronic) of the VASP calculation.
+
+        Returns:
+            bool: True if the calculation has converged, False otherwise.
+        """
         return self.vasprun.converged
 
 
     def get_energy(self) -> float:
+        """
+        Return the final energy of the VASP calculation.
+
+        Returns:
+            float: The final energy of the VASP calculation.
+        """
         return self.vasprun.final_energy
 
 
